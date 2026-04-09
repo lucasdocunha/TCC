1b8f24ce4bbb40761dca1519f330c11494f2fee2
Lucas <c.oliveira25@pucpr.edu.br>
2026-04-09 18:26:32 -0300

    add: dataloader add

---
+++ src/data/data.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset
+import pandas as pd
+from PIL import Image
+import numpy as np
+from pathlib import Path
+from torchvision import transforms 
+
+class ImageDataset(Dataset):
+    def __init__(self, file:Path, transform:transforms):
+        self.df = pd.read_csv(file, sep=';', names=['path', 'label'], header=None)
+        
+        #padrão da ImageNET -> outros modelos feitos em demais bases, possuem outros
+        self.transform = transform if transform else transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )
+        ])
+        
+    def __len__(self):
+        return len(self.df)
+    
+    
+    #crio um iterador para pegar a imagem, label e a posição do dado
+    def __getitem__(self, idx):
+        img_path = self.df.iloc[idx, 0]
+        with Image.open(img_path) as img:
+            img = img.convert('RGB')
+            
+        label = self.df.iloc[idx, 1]
+        
+        image = self.transform(img)
+        
+        return np.array(image), label, idx
+    
+
+
+        
