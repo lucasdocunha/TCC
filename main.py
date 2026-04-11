81714d39ba7c04203d5aee6b875aac43d55b075a
lucasdocunha <lucas.ocunha@ppgia.pucpr.br>
2026-04-11 12:50:49 -0300

    fix: corrigindo pipeline

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.data.data import ImageDataset
+from src.pipelines.xcpetion import run_xception
 from pathlib import Path
 from torchvision import transforms
 import os
@@ -8,14 +8,7 @@ PWD = Path.cwd()
 
 def main():
 
-    data_img = '/media/ssd2/lucas.ocunha/datasets/phase1/testset'
-
-    dataset = ImageDataset(
-        file_csv=f'{PWD}/data/raw/test.csv',
-	images_dir=data_img
-    )
-
-    print(dataset[0])
+    run_xception()
 
 
 if __name__ == "__main__":
