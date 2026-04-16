0c3015d5c635c86a36c47112db2b24862b88415c
Eduardo Netto <dudupnetto.en@gmail.com>
2026-04-16 19:42:52 -0300

    feat(main): Adicionando primeira versão do VIT

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.pipelines.xcpetion import run_xception
+from src.pipelines.vit import run_vit
 
 if __name__ == "__main__":
     modes_fourier = ["none", "magnitude", "phase", "complex", "concat"]
 
     for mode_fourier in modes_fourier:
         run_xception(mode_fourier, epochs=2)
+
+    run_vit()
