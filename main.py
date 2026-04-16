36c160cecda6766cbf6370651d081c7383413e4a
Eduardo Netto <dudupnetto.en@gmail.com>
2026-04-16 20:17:28 -0300

    chore(main): Tirando comentário do main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.pipelines.xcpetion import run_xception
 from src.pipelines.vit import run_vit
 
 if __name__ == "__main__":
-    # modes_fourier = ["none", "magnitude", "phase", "complex", "concat"]
+    modes_fourier = ["none", "magnitude", "phase", "complex", "concat"]
 
-    # for mode_fourier in modes_fourier:
-    #     run_xception(mode_fourier, epochs=2, raw_min=True)
+    for mode_fourier in modes_fourier:
+        run_xception(mode_fourier, epochs=2, raw_min=True)
 
     run_vit()
